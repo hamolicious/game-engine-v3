5811487ac8856be7f7f91010e2217d21965c252e
hamolicious <hamolicious@hamolicious.net>
2025-03-17 21:25:31 +0000

    make fsm a module

--- game/skeleton.py
+++ game/skeleton.py
@@ -1,7 +1,7 @@
 from enum import Enum, auto
 
-from engine.builtin_components import FiniteStateMachine, state
 from engine.builtin_components.transform import Transform2D
+from engine.fsm import FiniteStateMachine, state
 
 
 class State(Enum):
@@ -11,9 +11,9 @@ class State(Enum):
 
 class SkeletonFSM(FiniteStateMachine):
     @state(State.IDLE)
-    def idle(self, transform: Transform2D) -> Enum | None:
+    def idle(self, transform: Transform2D) -> State:
         return State.IDLE
 
     @state(State.RESTING)
-    def resting(self) -> Enum | None:
+    def resting(self) -> State:
         return State.RESTING
