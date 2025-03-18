83c4f4fe9be0128b6b173a6847e47096a6e5e4a7
Viktors Patmalnieks <viktorspatmalnieks@comparethemarket.com>
2025-03-18 10:40:58 +0000

    chase behaviour

--- game/skeleton.py
+++ game/skeleton.py
@@ -1,4 +1,5 @@
 from enum import Enum, auto
+from time import time
 
 from engine.builtin_components import Wandering, Detection, FollowPlayer
 from engine.fsm import FiniteStateMachine, state
@@ -6,22 +7,28 @@ from engine.fsm import FiniteStateMachine, state
 
 class State(Enum):
     IDLE = auto()
-    RESTING = auto()
     CHASING = auto()
 
 
 class SkeletonFSM(FiniteStateMachine):
+    def __init__(self, start_state: Enum) -> None:
+        super().__init__(start_state)
+
+        self.chase_started: float = 0
+        self.chase_time: float = 5
+
     @state(handles=State.IDLE, disables=(FollowPlayer,))
     def idle(self, detect: Detection) -> State:
         if len(detect.in_range) > 0:
+            self.chase_started = time()
             return State.CHASING
 
         return State.IDLE
 
     @state(handles=State.CHASING, disables=(Wandering,))
     def chasing(self) -> State:
+        if time() > self.chase_started + self.chase_time:
+            return State.IDLE
+
         return State.CHASING
 
-    @state(handles=State.RESTING)
-    def resting(self) -> State:
-        return State.RESTING
