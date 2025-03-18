d8a6406d44a1959e21d5c20ca2c077d3536b3af1
hamolicious <hamolicious@hamolicious.net>
2025-03-18 22:47:38 +0000

    export inits

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,2 +1,9 @@
-from . import builtin_components
+from pygame.math import Vector2
+
+from . import builtin_components, builtin_renderers, internal_components
+from .component import Component, ComponentTemplate
+from .ecs import ECSManager, Entity, EntityId
 from .engine import Engine
+from .fsm import FiniteStateMachine, state
+from .scene import Scene, SceneSetup
+from .system import system
--- engine/scene.py
+++ engine/scene.py
@@ -1,18 +1,13 @@
-from typing import Generator
+from typing import Generator, TypeAlias
 
-from .entity import Entity
+from engine.entity import Entity
+
+SceneSetup: TypeAlias = Generator[Entity, None, None]
 
 
 class Scene:
     def __init__(self) -> None:
-        self._entities: dict[str, Entity] = {}
+        pass
 
-    def add_entity(self, ent: Entity) -> None:
-        self._entities[ent.id] = ent
-
-    def get_entity(self, id: str) -> Entity | None:
-        return self._entities.get(id, None)
-
-    def get_all_entities(self) -> Generator[Entity, None, None]:
-        for ent in self._entities.values():
-            yield ent
+    def setup(self) -> SceneSetup:
+        raise NotImplementedError()
